94422cf1988e1f8746b131c59aa8ba773374abd4
CodingAdventure123 <105638595+CodingAdventure123@users.noreply.github.com>
2023-04-26 22:10:46 +0200

    Added current date and year

--- src/app.js
+++ src/app.js
@@ -1,5 +1,27 @@
 //Weather API SheCodes https://www.shecodes.io/learn/apis/weather
 
+function formatYear() {
+  let currentDate = new Date();
+  let day = currentDate.getDate();
+  let year = currentDate.getFullYear();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let month = months[currentDate.getMonth()];
+  return `${day}. ${month} ${year}`;
+}
+
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -30,6 +52,7 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let weekDayElement = document.querySelector("#week-day");
+  let dateYearElement = document.querySelector("#date-year");
 
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
@@ -37,9 +60,11 @@ function displayTemperature(response) {
   humidityElement.innerHTML = response.data.temperature.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   weekDayElement.innerHTML = formatDate(response.data.time * 1000);
+  dateYearElement.innerHTML = formatYear(response.data);
 }
 
 let apiKey = "bdbfbt8caf94d99e3db801476bbo7302";
+let city = "London";
 let apiUrl =
   "https://api.shecodes.io/weather/v1/current?query=London&key=bdbfbt8caf94d99e3db801476bbo7302&units=metric";
 
