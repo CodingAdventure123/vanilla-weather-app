77c893d32d0dc2edc84be881355d3833b0def1c9
CodingAdventure123 <105638595+CodingAdventure123@users.noreply.github.com>
2023-04-26 20:47:19 +0200

    Added wind element and humidity element

--- src/app.js
+++ src/app.js
@@ -4,9 +4,14 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   cityElement.innerHTML = response.data.city;
   descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = response.data.temperature.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 let apiKey = "bdbfbt8caf94d99e3db801476bbo7302";
